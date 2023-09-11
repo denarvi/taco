ab1ec5e1f46cb7559f5ed8545fad2164ff2a2f65
Aravind <denarvi@gmail.com>
2023-09-11 18:13:47 -0400

    TACO code

---
+++ TacoLLM/constants.py
@@ -0,0 +1,1 @@
+credentials_json = "participant-sa-15-ghc-011.json"
---
+++ app.py
@@ -0,0 +1,34 @@
+import os
+
+from dotenv import load_dotenv
+from Utility import ResponseTemplates
+from flask import Flask, request
+from flask_cors import CORS
+from TacoLLM import RequestHandler
+from Utility.AthenticationToken import require_token
+
+app = Flask(__name__)
+CORS(app)
+
+load_dotenv()
+
+
+@app.route("/")
+def main():
+    return ResponseTemplates.getLandingPage()
+
+
+@app.route("/help", methods=['GET'])
+def taco_help():
+    return ResponseTemplates.getHelp()
+
+
+@app.route("/taco-request", methods=['POST'])
+@require_token
+def taco_request():
+    inputQuery = request.form.get("message")
+    return RequestHandler.handleRequest(inputQuery)
+
+
+if __name__ == '__main__':
+    app.run()
